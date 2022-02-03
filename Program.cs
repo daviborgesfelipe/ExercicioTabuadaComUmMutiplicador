90e2c4c14e71c987a126a2ddada9b16cfcc60f30
Davi Borges <davifelipe_300@hotmail.com>
2022-02-03 17:32:46 -0300

    Exercicio de Tabuada Multiplicador e Multiplicando

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*Desenvolva um programa que solicite o multiplicador ao usuário e apresente o resultado da multiplicação de 0 a 10*/
+
+using System;
+
+namespace ExercicioTabuadaComUmMutiplicador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicador;
+            int multiplicando = 0;
+            int resultado; 
+
+            Console.WriteLine("Digite o multiplicador: ");
+            multiplicador = Convert.ToInt32(Console.ReadLine());
+
+           
+            
+            while(multiplicando <= 10)
+            {
+                resultado = multiplicando * multiplicador;
+                Console.WriteLine($"{multiplicador} x {multiplicando} = {resultado}");
+                multiplicando = multiplicando + 1;
+            }
+        }
+    }
+}
